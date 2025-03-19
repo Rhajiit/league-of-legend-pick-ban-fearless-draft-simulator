3762f4038701c372bee135f4dae5458c75471c79
Rhajiit <ksd9638@gmail.com>
2025-03-19 15:36:31 +0900

    💄 챔피언 초상화에 픽밴 유무를 표시하는 간이 UI 적용

--- src/components/portrait.tsx
+++ src/components/portrait.tsx
@@ -5,13 +5,23 @@ const ChampionPortrait = ({
 }: {
   championData: ChampionDataType;
 }) => {
+  const selectedChampion = championData.name === "Ahri" ? true : false;
+  const bannedChampion = championData.name === "Aatrox" ? true : false;
+
   return (
-    <div className="flex max-w-20 cursor-default flex-col items-center bg-white">
+    <div className="relative flex max-w-20 cursor-default flex-col items-center bg-white">
       <img
-        className="h-20 w-20 cursor-pointer"
+        className={`h-20 w-20 ${selectedChampion || bannedChampion ? "cursor-default grayscale" : "cursor-pointer"}`}
         src={`/assets/champion/${championData.image.full}`}
       />
-      <p className="max-w-full truncate">{championData.name}</p>
+      {bannedChampion && (
+        <div className="absolute h-20 w-20 border-4 border-red-600"></div>
+      )}
+      <p
+        className={`max-w-full truncate ${bannedChampion ? "text-red-600" : ""}`}
+      >
+        {championData.name}
+      </p>
     </div>
   );
 };
