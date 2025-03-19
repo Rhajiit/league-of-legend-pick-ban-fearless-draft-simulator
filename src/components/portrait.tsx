e665d5260d6478ae1fae9e2cd73926523fed8d2a
Rhajiit <ksd9638@gmail.com>
2025-03-19 14:45:54 +0900

    🐛 assets 불러오는 경로를 vite에서 제시하는 대로 변경
    /public/assets/... 에서 /assets/...으로 변경

--- src/components/portrait.tsx
+++ src/components/portrait.tsx
@@ -9,7 +9,7 @@ const ChampionPortrait = ({
     <div className="flex max-w-20 cursor-default flex-col items-center bg-white">
       <img
         className="h-20 w-20 cursor-pointer"
-        src={`/public/assets/champion/${championData.image.full}`}
+        src={`/assets/champion/${championData.image.full}`}
       />
       <p className="max-w-full truncate">{championData.name}</p>
     </div>
