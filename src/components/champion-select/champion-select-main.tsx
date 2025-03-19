373962e47fc25b7f9df7da6bc131d10cc5e94b51
Rhajiit <ksd9638@gmail.com>
2025-03-19 15:36:01 +0900

    🎨 챔피언 선택 화면을 컴포넌트로 분리하여 코드 가독성 및 재사용성 증대
    추후 서버 관련 기능을 활용할 때 재사용이 용이하도록 구조 개편함

---
+++ src/components/champion-select/champion-select-main.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+
+import {
+  ChampionDataListType,
+  ChampionTagType,
+} from "../../types/championDataType";
+import ChampionFilterByTag from "./champion-filter";
+import ChampionPortrait from "../portrait";
+import { championTypes } from "../../utils/constants/champion-type";
+
+interface ComponentChampionSelectType {
+  selectedChampionList: string[];
+  bannedChampionList: string[];
+  championData: ChampionDataListType;
+}
+
+const ChampionSelect = ({
+  selectedChampionList,
+  bannedChampionList,
+  championData,
+}: ComponentChampionSelectType) => {
+  const [tagFilter, setTagFilter] = useState<ChampionTagType>(null);
+  const [nameFilter, setNameFilter] = useState<string>("");
+  const [timer, setTimer] = useState<number>(90);
+
+  const handleChampionTagFilter = (selectedTagFilter: ChampionTagType) => {
+    if (tagFilter === selectedTagFilter) {
+      setTagFilter(null);
+    } else {
+      setTagFilter(selectedTagFilter);
+    }
+  };
+
+  return (
+    <div className="w-full border">
+      <header className="flex flex-col">
+        <div className="text-center">
+          <p>희망 챔피언을 선택하세요!</p>
+          <div>{timer}</div>
+        </div>
+
+        <div className="border">
+          <div className="flex justify-between">
+            <div>
+              {championTypes.map((tag) => (
+                <ChampionFilterByTag
+                  key={`champion-filter-button-${tag}`}
+                  tagFilter={tag}
+                  onFilterChampionTag={handleChampionTagFilter}
+                />
+              ))}
+            </div>
+
+            <input
+              type="text"
+              onChange={(event) => setNameFilter(event.target.value)}
+              className="m-1 h-full border"
+            />
+          </div>
+        </div>
+      </header>
+      <div className="h-full overflow-y-scroll">
+        <main className="m-auto my-4 grid w-fit grid-cols-6 gap-x-12 gap-y-1">
+          {Object.keys(championData).map((championName) => {
+            return tagFilter === null && nameFilter === "" ? (
+              <ChampionPortrait
+                key={`champion-id-${championData[championName].key}`}
+                championData={championData[championName]}
+              />
+            ) : (
+              championName.toLowerCase().includes(nameFilter.toLowerCase()) &&
+                (tagFilter === null ||
+                  championData[championName].tags.includes(tagFilter)) && (
+                  <ChampionPortrait
+                    key={`champion-id-${championData[championName].key}`}
+                    championData={championData[championName]}
+                  />
+                )
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default ChampionSelect;
--- src/pages/local-pb.tsx
+++ src/pages/local-pb.tsx
@@ -1,80 +1,23 @@
 import { useState } from "react";
-import ChampionPortrait from "../components/portrait";
+import ChampionSelect from "../components/champion-select/champion-select-main";
 import championRawData from "../mocks/champion.json";
-import {
-  ChampionDataListType,
-  ChampionTagType,
-} from "../types/championDataType";
-import { championTypes } from "../utils/constants/champion-type";
-import ChampionFilterByTag from "../components/champion-select/champion-filter";
+import { ChampionDataListType } from "../types/championDataType";
 
 const championData: ChampionDataListType = championRawData.data;
 
 const LocalPB = () => {
-  const [tagFilter, setTagFilter] = useState<ChampionTagType>(null);
-  const [nameFilter, setNameFilter] = useState<string>("");
-  const [timer, setTimer] = useState<number>(90);
-
-  const handleChampionTagFilter = (selectedTagFilter: ChampionTagType) => {
-    if (tagFilter === selectedTagFilter) {
-      setTagFilter(null);
-    } else {
-      setTagFilter(selectedTagFilter);
-    }
-  };
+  const [bannedList, setBannedList] = useState<string[]>([]);
+  const [selectedList, setSelectedList] = useState<string[]>([]);
 
   return (
     <div className="flex h-screen overflow-hidden">
       <div className="min-w-96 border">Blue Team</div>
-      <div className="w-full border">
-        <header className="flex flex-col">
-          <div className="text-center">
-            <p>희망 챔피언을 선택하세요!</p>
-            <div>{timer}</div>
-          </div>
+      <ChampionSelect
+        selectedChampionList={selectedList}
+        bannedChampionList={bannedList}
+        championData={championData}
+      />
 
-          <div className="border">
-            <div className="flex justify-between">
-              <div>
-                {championTypes.map((tag) => (
-                  <ChampionFilterByTag
-                    key={`champion-filter-button-${tag}`}
-                    tagFilter={tag}
-                    onFilterChampionTag={handleChampionTagFilter}
-                  />
-                ))}
-              </div>
-
-              <input
-                type="text"
-                onChange={(event) => setNameFilter(event.target.value)}
-                className="m-1 h-full border"
-              />
-            </div>
-          </div>
-        </header>
-        <div className="h-full overflow-y-scroll">
-          <main className="m-auto my-4 grid w-fit grid-cols-6 gap-x-12 gap-y-1">
-            {Object.keys(championData).map((championName) => {
-              return tagFilter === null && nameFilter === "" ? (
-                <ChampionPortrait
-                  key={`champion-id-${championData[championName].key}`}
-                  championData={championData[championName]}
-                />
-              ) : (
-                championName.toLowerCase().includes(nameFilter.toLowerCase()) &&
-                  (tagFilter === null ||
-                    championData[championName].tags.includes(tagFilter)) && (
-                    <ChampionPortrait
-                      key={`champion-id-${championData[championName].key}`}
-                      championData={championData[championName]}
-                    />
-                  )
-              );
-            })}
-          </main>
-        </div>
-      </div>
       <div className="min-w-96 border">Red Team</div>
     </div>
   );
