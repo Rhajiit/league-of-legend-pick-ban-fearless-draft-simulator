9e4079ad67cda301f5f99f4e5fc08a2c1e44078a
Rhajiit <ksd9638@gmail.com>
2025-03-19 17:56:36 +0900

    💄 챔피언 선택을 알려주는 임시 요소 생성

--- src/components/portrait.tsx
+++ src/components/portrait.tsx
@@ -7,9 +7,10 @@ const ChampionPortrait = ({
 }) => {
   const selectedChampion = championData.name === "Ahri" ? true : false;
   const bannedChampion = championData.name === "Aatrox" ? true : false;
+  const focusedChampion = championData.name === "Akali" ? true : false;
 
   return (
-    <div className="relative flex max-w-20 cursor-default flex-col items-center bg-white">
+    <div className="relative flex w-20 cursor-default flex-col items-center bg-white">
       <img
         className={`h-20 w-20 ${selectedChampion || bannedChampion ? "cursor-default grayscale" : "cursor-pointer"}`}
         src={`/assets/champion/${championData.image.full}`}
@@ -20,6 +21,9 @@ const ChampionPortrait = ({
       <p
         className={`max-w-full truncate ${bannedChampion ? "text-red-600" : ""}`}
       >
+        {focusedChampion && (
+          <div className="absolute top-1 left-1 h-18 w-18 rounded-full border-8 border-cyan-800"></div>
+        )}
         {championData.name}
       </p>
     </div>
